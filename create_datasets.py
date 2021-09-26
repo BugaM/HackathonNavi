56c0eb7424738cdeb62541fcf4d74b5e4d08ab3b
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-09-26 19:55:18 -0300

    feat: add file that creates company and industry dataset

---
+++ create_datasets.py
@@ -0,0 +1,7 @@
+print("Creating dataset based on S&P ESG and Risk dataset and Yahoo Finance API of Companies listed in IBOVESPA.")
+print("It may take a while, because of the API.")
+import prototype_data
+print("Company dataset successfully created.")
+print("Creating dataset for ESG and risk of each industry.")
+import industries_data
+print("Industry dataset successfully created.")
