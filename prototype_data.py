535a81a890ad8417940067b195ff3d2933048d04
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-09-26 13:45:51 -0300

    feat: add stock pricing

--- prototype_data.py
+++ prototype_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from yahoo_fin import stock_info
+import cufflinks as cf
+cf.set_config_file(offline=True)
 
 df_esg = pd.read_csv("data/esg_scores_history_br.csv")
 df_risk = pd.read_csv("data/physical_risk_forecast_br.csv")
@@ -20,22 +23,27 @@ df_risk = df_risk[df_risk.forecast_year == 2050]
 df_risk = df_risk[df_risk.scenario_level == 'High']
 risk = []
 ticker = []
+price = []
 for _, row in dataset.iterrows():
     value = df_risk[df_risk.company_id == row.company_id]
     if not value.empty:
         risk.append(value.data_item_value.values[0])
     else:
         risk.append(np.NAN)
-
+dataset['risk'] = risk
 for _, row in dataset.iterrows():
     value = df_company[df_company.company_id == row.company_id]
     if not value.empty:
         ticker.append(value.ticker.values[0])
     else:
         ticker.append(np.NAN)
-
-dataset['risk'] = risk
 dataset['ticker'] = ticker
+for _, row in dataset.iterrows():
+    try:
+        price.append(stock_info.get_live_price((row.ticker+".SA")))
+    except:
+        price.append(np.NAN)
+dataset['price'] = price
 dataset = dataset.dropna()
 dataset = dataset.drop_duplicates(keep='first')
 dataset.to_csv('data/selected_prototype_data.csv', index=False)
