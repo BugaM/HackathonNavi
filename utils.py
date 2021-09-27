48b5e012bdfbba0e13e45d6be594e549180524ff
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-09-26 21:35:43 -0300

    fix: remove unecessary files

--- utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-# used to reduce datasets
-
-csv = pd.read_csv('data/environmental_data_history_rated.csv')
-csv = csv.drop(csv[csv.fiscal_year < 2012].index)
-
-
-csv.to_csv('environmental_data_history_rated2012.zip', index=False)
